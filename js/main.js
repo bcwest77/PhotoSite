bac5dd026d53cbfc9f8fdda7483cac9f877d0333
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-01 00:32:31 -0500

    z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 function changeImage(imgSrc) {
     document.getElementById("modal-img").src = imgSrc;
 }
+
+function changeTheme() {
+    if (theme == "default") {
+        invertTheme();
+    }
+    else
+    {
+        defaultTheme();
+    }
+}
+function defaultTheme() {
+    theme = "default";
+    document.cookie = "theme=default";
+
+    document.body.className = oldBodyClass;
+}
+function invertTheme() {
+    theme = "inverted";
+    document.cookie = "theme=inverted";
+    
+    document.body.className = "inverted";
+}
